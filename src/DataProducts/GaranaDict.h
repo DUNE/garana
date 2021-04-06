48ad8d4d45e098fb573a033699ada880f0f4b7b3
Chris Hilgenberg <chilgenb@umn.edu>
2021-04-05 21:06:31 -0500

    turns out you need this to keep this version of dictionary generation happy

---
+++ src/DataProducts/GaranaDict.h
@@ -0,0 +1,23 @@
+//
+// Build a dictionary.
+//
+
+#ifndef GARANADICT_H
+#define GARANADICT_H
+
+// garana
+#include "garana/DataProducts/GTruth.h"
+#include "garana/DataProducts/FSParticle.h"
+#include "garana/DataProducts/G4Particle.h"
+#include "garana/DataProducts/Track.h"
+#include "garana/DataProducts/Vertex.h"
+#include "garana/DataProducts/Vee.h"
+#include "garana/DataProducts/CaloCluster.h"
+
+// root
+#include <TLorentzVector.h>
+
+// c++
+#include <vector>
+
+#endif /* classes_h */
