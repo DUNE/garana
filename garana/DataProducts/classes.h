79da98d9ec804c543905db5c346b46e8532b01b0
Chris Hilgenberg <chilgenb@umn.edu>
2021-05-12 19:20:02 -0500

    missed a few more

---
+++ garana/DataProducts/classes.h
@@ -0,0 +1,28 @@
+//
+// Build a dictionary.
+//
+
+#ifndef CLASSES_H
+#define CLASSES_H
+
+//#if BUILD_W_CET
+//#include "canvas/Persistency/Common/Wrapper.h"
+//#endif
+
+// garana
+#include "garana/DataProducts/GTruth.h"
+#include "garana/DataProducts/FSParticle.h"
+#include "garana/DataProducts/G4Particle.h"
+#include "garana/DataProducts/Track.h"
+#include "garana/DataProducts/Vertex.h"
+#include "garana/DataProducts/Vee.h"
+#include "garana/DataProducts/CaloCluster.h"
+
+// root
+#include <TLorentzVector.h>
+
+// c++
+#include <vector>
+#include <utility>
+
+#endif /* CLASSES_H */
